4511e8bcdaa3f21cd7b69d8679bc3a6c86d3f4f0
Clément Girard <66427214+Skyedge1903@users.noreply.github.com>
2025-04-03 23:51:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -137,6 +137,6 @@ def favicon():
 def serve_root():
     return flask.redirect('/accueil')
 
+
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8050))
-    app.run_server(host='0.0.0.0', port=port, debug=False)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8050)))
